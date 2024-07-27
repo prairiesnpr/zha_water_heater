077c1f6766cc51d12813f401c5c72237f5017e57
PrairieSnpr <midnryanmiller@gmail.com>
2024-07-27 12:28:58 -0400

    Got carried away

--- zha/device_details.h
+++ zha/device_details.h
@@ -24,17 +24,17 @@ static attribute out_temp_attr[] = {{CURRENT_STATE, 0x0000, 2, ZCL_INT16_T}};
 
 // dev_basic_attr
 
-static Cluster s_in_clusters[] = {Cluster(BASIC_CLUSTER_ID, dev_basic_attr, sizeof(dev_basic_attr)),
-                                  Cluster(ON_OFF_CLUSTER_ID, ssr_attr, sizeof(ssr_attr)),
-                                  Cluster(ELECTRICAL_MEASUREMENT, metering_attr, sizeof(metering_attr))};
-static Cluster i_in_clusters[] = {Cluster(BASIC_CLUSTER_ID, in_temp_basic_attr, sizeof(in_temp_basic_attr)),
-                                  Cluster(TEMP_CLUSTER_ID, in_temp_attr, sizeof(in_temp_attr))};
-static Cluster o_in_clusters[] = {Cluster(BASIC_CLUSTER_ID, out_temp_basic_attr, sizeof(out_temp_basic_attr)),
-                                  Cluster(TEMP_CLUSTER_ID, out_temp_attr, sizeof(out_temp_attr))};
+static Cluster s_in_clusters[] = {Cluster(BASIC_CLUSTER_ID, dev_basic_attr, sizeof(dev_basic_attr) / sizeof(*dev_basic_attr)),
+                                  Cluster(ON_OFF_CLUSTER_ID, ssr_attr, sizeof(ssr_attr) / sizeof(*ssr_attr)),
+                                  Cluster(ELECTRICAL_MEASUREMENT, metering_attr, sizeof(metering_attr) / sizeof(*metering_attr))};
+static Cluster i_in_clusters[] = {Cluster(BASIC_CLUSTER_ID, in_temp_basic_attr, sizeof(in_temp_basic_attr) / sizeof(*in_temp_basic_attr)),
+                                  Cluster(TEMP_CLUSTER_ID, in_temp_attr, sizeof(in_temp_attr) / sizeof(*in_temp_attr))};
+static Cluster o_in_clusters[] = {Cluster(BASIC_CLUSTER_ID, out_temp_basic_attr, sizeof(out_temp_basic_attr) / sizeof(*out_temp_basic_attr)),
+                                  Cluster(TEMP_CLUSTER_ID, out_temp_attr, sizeof(out_temp_attr) / sizeof(*out_temp_attr))};
 
 static Cluster out_clusters[] = {};
 static Endpoint ENDPOINTS[NUM_ENDPOINTS] = {
-    Endpoint(1, ON_OFF_OUTPUT, s_in_clusters, out_clusters, sizeof(s_in_clusters), 0),
-    Endpoint(2, TEMPERATURE_SENSOR, i_in_clusters, out_clusters, sizeof(i_in_clusters), 0),
-    Endpoint(3, TEMPERATURE_SENSOR, o_in_clusters, out_clusters, sizeof(o_in_clusters), 0),
+    Endpoint(1, ON_OFF_OUTPUT, s_in_clusters, out_clusters, sizeof(s_in_clusters) / sizeof(*s_in_clusters), 0),
+    Endpoint(2, TEMPERATURE_SENSOR, i_in_clusters, out_clusters, sizeof(i_in_clusters) / sizeof(*i_in_clusters), 0),
+    Endpoint(3, TEMPERATURE_SENSOR, o_in_clusters, out_clusters, sizeof(o_in_clusters) / sizeof(*o_in_clusters), 0),
 };
